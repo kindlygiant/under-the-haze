0115525526132c4b3c37898d3ff20f15d45af91f
rwright <rwright@lillypulitzer.com>
2018-10-25 11:31:37 -0400

    init

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//Styling
+import './App.scss';
+
+//Components
+
+class App extends React.Component{
+    render(){
+        return (
+            <h1>Hello World!</h1>
+        );
+    }
+}
+
+export default App;
