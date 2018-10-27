a9be2ed3c647e50735ccf40ff96ede0fdf57472d
DekuKnight <rdwright5127@gmail.com>
2018-10-27 10:41:19 -0400

    redacted information

--- src/containers/Donate/Donate.js
+++ src/containers/Donate/Donate.js
@@ -12,8 +12,8 @@ class Donate extends React.Component{
                     Right now we only take donations in the form of cryptic and mildly threatening notes made from newspaper and magazine clippings sent to this address:
                 </p>
                 <div className="address">
-                    <p>2213 33rd St NW Apt 207</p>
-                    <p>58703, Minot ND</p>
+                    <p>REDACTED</p>
+                    <p>REDACTED</p>
                 </div>
                 <p>
                     Please don't put a return address.
