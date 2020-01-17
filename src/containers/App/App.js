6a7afc30733ea913ceb2483da00205fd753f6b3d
rwright <rdwright5127@gmail.com>
2020-01-17 16:27:40 -0500

    Content Updates
    Added new Pages
    Filled new pages with dummy text

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -9,8 +9,11 @@ import Header from '../../components/Header/Header';
 import Home from '../Home/Home';
 import Overview from '../Overview/Overview';
 import Episodes from '../Episodes/Episodes';
-import Donate from '../Donate/Donate';
 import Music from '../Music/Music';
+import About from '../About/About';
+import Donate from '../Donate/Donate';
+import Listen from '../Listen/Listen';
+import Contact from '../Contact/Contact';
 
 class App extends React.Component{
     render(){
@@ -24,7 +27,10 @@ class App extends React.Component{
                         <Route path="/overview" component={Overview} />
                         <Route path="/episodes" component={Episodes} />
                         <Route path="/music" component={Music} />
+                        <Route path="/about" component={About} />
                         <Route path="/donate" component={Donate} />
+                        <Route path="/listen" component={Listen} />
+                        <Route path="/contact" component={Contact} />
                     </main>
                 </div>
             </Router>
---
+++ src/containers/Contact/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//Styling
+import './Contact.scss';
+
+//Components
+
+class Contact extends React.Component{
+    render(){
+        return (
+            <div className="overview">
+                <p>
+                    We have an email. You could try that I guess...
+                </p>
+                <p>
+                    underthehazecast@gmail.com
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
